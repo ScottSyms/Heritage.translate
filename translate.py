0c9eb1d6c93c4a391c708a695e01fde468e9b7c3
Scott Syms <scottsyms@SpottedFish.local>
2022-12-30 19:20:17 -0400

    broke up the translation block into sentences

--- translate.py
+++ translate.py
@@ -14,7 +14,6 @@ warnings.filterwarnings("ignore")
 
 os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "1"
 
-
 def translate(text, model, tokenizer):
     print(" Removing newlines ...  ")
     print(" Number of words: ", len(text.split()))
@@ -22,45 +21,47 @@ def translate(text, model, tokenizer):
     sentences = text.split('\n')
     print(" Number of paragraphs: ", len(sentences))
 
-
-    # print("Sentences: ", sentences)
-    translated = model.generate(
-        **tokenizer(sentences, return_tensors="pt", padding=True).to(torch.device))
-    print("Decoding...")
-    test = [tokenizer.decode(t, skip_special_tokens=True) for t in translated]
-    return "\n".join(test)
-
+    print("Sentences: ", sentences)
+    translation_output=[]
+    for i in sentences:
+        print(" Sentence: ", i)
+        print(" Sentence length: ", len(i.split()))
+        translated = model.generate(
+            **tokenizer(i, return_tensors="pt", padding=True))
+        print("Decoding...")
+        translation_output.append([tokenizer.decode(t, skip_special_tokens=True) for t in translated][0])
+    print(translation_output)
+    return "\n".join(translation_output)
 
 if torch.cuda.is_available():
     dev = "cuda"
     print("Using CUDA")
 elif torch.has_mps:
-    dev = "mps"
+    dev = "cpu"
     # torch.device = "cpu"
     print("Using Apple MPS")
 else:
     dev = "cpu"
     print("Using CPU")
 
-torch.device(dev)
+device = torch.device(dev)
 
 print("Loading models ...  ")
 fr_en_tokenizer = MarianTokenizer.from_pretrained("Helsinki-NLP/opus-mt-fr-en")
 fr_en_model = MarianMTModel.from_pretrained("Helsinki-NLP/opus-mt-fr-en")
-fr_en_model.to(torch.device)
+fr_en_model.to(device)
 
 en_fr_tokenizer = MarianTokenizer.from_pretrained("Helsinki-NLP/opus-mt-en-fr")
 en_fr_model = MarianMTModel.from_pretrained("Helsinki-NLP/opus-mt-en-fr")
-en_fr_model.to(torch.device)
+en_fr_model.to(device)
 
 en_es_tokenizer = MarianTokenizer.from_pretrained("Helsinki-NLP/opus-mt-en-es")
 en_es_model = MarianMTModel.from_pretrained("Helsinki-NLP/opus-mt-en-es")
-en_es_model.to(torch.device)
+en_es_model.to(device)
 
 fr_es_tokenizer = MarianTokenizer.from_pretrained("Helsinki-NLP/opus-mt-fr-es")
 fr_es_model = MarianMTModel.from_pretrained("Helsinki-NLP/opus-mt-fr-es")
-fr_es_model.to(torch.device)
-
+fr_es_model.to(device)
 
 dbcon = create_engine(
     'sqlite:////Users/scottsyms/code/HeritageCanada/data/fish/sample2.db')
@@ -88,14 +89,14 @@ for i in selectid:
         translatealternate = source.update().values(french=translate(i[2], en_fr_model, en_fr_tokenizer)).where(
             source.c.id == selectid[count][0])
         print(" Translate to Spanish")
-        translatetospanish = source.update().values(spanish='Translation to Spanish').where(
+        translatetospanish = source.update().values(spanish=translate(i[2], en_es_model, en_es_tokenizer)).where(
             source.c.id == selectid[count][0])
     elif i[1] == 'fr':
         movelanguage = source.update().values(french=i[2]).where(
             source.c.id == selectid[count][0])
-        translatealternate = source.update().values(english='Translation to English').where(
+        translatealternate = source.update().values(english=translate(i[2], fr_en_model, fr_en_tokenizer)).where(
             source.c.id == selectid[count][0])
-        translatetospanish = source.update().values(spanish='Translation to Spanish').where(
+        translatetospanish = source.update().values(spanish=translate(i[2], fr_es_model, fr_es_tokenizer)).where(
             source.c.id == selectid[count][0])
     dbcon.execute(movelanguage)
     dbcon.execute(translatealternate)
